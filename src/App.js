513dee48297fab38e34a41401da63272d8d7022a
fuernkaes.gloria@web.de <fuernkaes.gloria@web.de>
2024-12-19 15:20:15 +0100

    hashrouter statt browserrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import CV from "./CV"
 import Layout from "./Layout";
 import Home from "./Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
   
-    <BrowserRouter basename="/react-website">
+    <HashRouter basename="/react-website">
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>} />
         <Route path="cv" element={<CV />} />
       </Route>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
